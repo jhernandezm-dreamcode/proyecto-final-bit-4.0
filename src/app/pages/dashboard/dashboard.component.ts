bacf3f55e313724795789463e1f13e2b724a81d1
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-03-23 09:50:36 -0500

    delete attributes

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -7,12 +7,7 @@ import { Router } from '@angular/router';
   styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  public datasets: any;
-  public data: any;
-  public salesChart;
-  public clicked: boolean = true;
-  public clicked1: boolean = false;
-
+ 
   constructor(private router: Router) {}
 
   users: Array<any> = [];
