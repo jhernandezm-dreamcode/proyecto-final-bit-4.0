54ffa6db82898d6f1863f6c13997eeca50a1ded8
Jorge Hernandez <jorge.hernandez@dreamcodesoft.com>
2021-03-27 16:40:20 -0500

    m

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -11,12 +11,6 @@ export class DashboardComponent implements OnInit {
   constructor(private router: Router) {}
 
   users: Array<any> = [];
-  user1 = {
-    name: 'Jorge Arturo',
-    lastname: 'Hernandez',
-    age: '25',
-    survayStatuts: 'Pendiente',
-  };
   ngOnInit() {
     this.users = [
       {
@@ -24,17 +18,51 @@ export class DashboardComponent implements OnInit {
         lastname: 'Hernandez',
         age: '25',
         survayStatuts: 'Pendiente',
+        company: 'Dreamcode',
+        arl:'Positiva',
+        documentNumber:"1222212313",
+        area:"Desarrollo",
+        address:"Cali",
+        phoneNumber:'121212',
+        charge:"Desarrollador de software",
+        gender:"Masculino"
       },
       {
         name: 'Esteban',
         lastname: 'Camacho',
         age: '28',
         survayStatuts: 'Completada',
+        company: 'Dreamcode',
+        arl:'Positiva',
+        documentNumber:"1223456",
+        area:"Desarrollo",
+        address:"Buga",
+        phoneNumber:'11123232',
+        charge:"Desarrollador de software",
+        gender:"Masculino"
+      },
+      {
+        name: 'Andres',
+        lastname: 'Perez',
+        age: '26',
+        survayStatuts: 'Completada',
+        company: 'Dreamcode',
+        arl:'Positiva',
+        documentNumber:"998989098",
+        area:"Desarrollo",
+        address:"Yumbo",
+        phoneNumber:'00988',
+        charge:"Desarrollador de software",
+        gender:"Masculino"
       },
     ];
   }
 
   goToInfoUsuario(user: any) {
     this.router.navigate(['/user-detail'], { queryParams: user });
+  }
+
+  getPendingSurveys(status:string){
+    return this.users.filter(x=> x.survayStatuts ===status).length
   }
 }
--- src/app/pages/user-detail/user-detail.component.ts
+++ src/app/pages/user-detail/user-detail.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-
 @Component({
   selector: 'app-user-detail',
   templateUrl: './user-detail.component.html',
   styleUrls: ['./user-detail.component.css'],
 })
 export class UserDetailComponent implements OnInit {
+  user:any;
   constructor(private router: Router, private route: ActivatedRoute) {
-    route.queryParams.subscribe((params) => {console.log(params); console.log(params.name)});
+    route.queryParams.subscribe((params) => {console.log(params); this.user = params});
   }
-
+  saveInformation(){
+    this.router.navigate(['/dashboard']);
+  }
+  getFullName(){
+    return `${this.user.name} ${this.user.lastname}`;
+  }
   ngOnInit(): void {}
 }
